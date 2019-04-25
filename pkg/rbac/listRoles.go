f305ce716203ac28d4e82ca2877038b2af5729a7
Thorsten Kukuk <kukuk@thkukuk.de>
2019-04-25 15:58:48 +0200

    Add option to list current roles and users for RBAC

--- pkg/rbac/listRoles.go
+++ pkg/rbac/listRoles.go
@@ -15,20 +15,34 @@
 package rbac
 
 import (
+	"os"
+	"fmt"
+
         "github.com/spf13/cobra"
+	"gopkg.in/ini.v1"
 )
 
-func RBACCmd() *cobra.Command {
+func ListRolesCmd() *cobra.Command {
         var subCmd = &cobra.Command {
-		Use:   "rbac",
-                Short: "Manage RBAC rules",
+                Use:   "list",
+                Short: "List roles and accounts",
+                Run: listRoles,
+                Args: cobra.ExactArgs(0),
         }
 
-        subCmd.AddCommand(
-		AddAccountCmd(),
-//                RemoveAccountCmd(),
-		ListRolesCmd(),
-        )
-
         return subCmd
+}
+
+func listRoles (cmd *cobra.Command, args []string) {
+	cfg, err := ini.LooseLoad("/usr/share/defaults/kubicd/rbac.conf", "/etc/kubicd/rbac.conf")
+        if err != nil {
+		fmt.Fprintf(os.Stderr, "Cannot load rbac.conf: %v\n", err)
+		os.Exit(1)
+	}
+
+	roleList := cfg.Section("").KeyStrings()
+	for i := range roleList {
+		entry := cfg.Section("").Key(roleList[i]).String()
+		fmt.Printf("%s: %s\n", roleList[i], entry)
+	}
 }
