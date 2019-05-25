thkukuk/kubic-control

commit eecc29e8c37db2d11012de083564db6211a7a3ae

Use cilium.yaml from master branch

pkg/kubeadm/initMaster.go
@@ -134,8 +134,7 @@ func InitMaster(in *pb.InitRequest, stream pb.Kubeadm_InitMasterServer) error {
 		if err := stream.Send(&pb.StatusReply{Success: true, Message: "Deploy cilium"}); err != nil {
 			return err
 		}
-		//		success, message = ExecuteCmd("kubectl", "--kubeconfig=/etc/kubernetes/admin.conf",  "apply", "-f", "https://raw.githubusercontent.com/kubic-project/k8s-manifests/cilium/cilium.yaml")
-		success, message = ExecuteCmd("kubectl", "--kubeconfig=/etc/kubernetes/admin.conf",  "apply", "-f", "https://raw.githubusercontent.com/kubic-project/k8s-manifests/65cc2ac79b2ed2448b366f9d89c1bf43e35c827f/cilium.yaml")
+		success, message = ExecuteCmd("kubectl", "--kubeconfig=/etc/kubernetes/admin.conf",  "apply", "-f", "https://raw.githubusercontent.com/kubic-project/k8s-manifests/cilium/cilium.yaml")
 		if success != true {
 			ResetMaster()
 			if err := stream.Send(&pb.StatusReply{Success: success, Message: message}); err != nil {

commit 23a69990912fb01aa06ff8f39c69eaf82d285f39

kubeadm: init: Use cilium k8s yaml from package
Cilium k8s yaml manifest is now available as a package which should be
installed as a part of kubeadm pattern.

Fixes: #1
Fixes: eecc29e8c37d ("Use cilium.yaml from master branch")

Signed-off-by: Michal Rostecki <mrostecki@opensuse.org>

pkg/kubeadm/initMaster.go
@@ -134,7 +134,7 @@ func InitMaster(in *pb.InitRequest, stream pb.Kubeadm_InitMasterServer) error {
 		if err := stream.Send(&pb.StatusReply{Success: true, Message: "Deploy cilium"}); err != nil {
 			return err
 		}
-		success, message = ExecuteCmd("kubectl", "--kubeconfig=/etc/kubernetes/admin.conf",  "apply", "-f", "https://raw.githubusercontent.com/kubic-project/k8s-manifests/cilium/cilium.yaml")
+		success, message = ExecuteCmd("kubectl", "--kubeconfig=/etc/kubernetes/admin.conf",  "apply", "-f", "/usr/share/k8s-yaml/cilium/cilium.yaml")
 		if success != true {
 			ResetMaster()
 			if err := stream.Send(&pb.StatusReply{Success: success, Message: message}); err != nil {