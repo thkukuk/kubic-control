7666189083107c58ef69968eacfdcc7e10dba2dc
Thorsten Kukuk <kukuk@suse.com>
2019-07-30 16:32:59 +0200

    Create initial haproxy.cfg

--- cmd/haproxycfg/init.go
+++ cmd/haproxycfg/init.go
@@ -16,21 +16,102 @@ package main
 
 import (
 	"fmt"
+	"os"
 
         "github.com/spf13/cobra"
 )
 
+var (
+	output_dir = "/etc/haproxy"
+)
+
 func InitializeConfigCmd() *cobra.Command {
         var subCmd = &cobra.Command {
-                Use:   "initialize",
+                Use:   "initialize <loadbalancer DNS name> <first master IP>",
                 Short: "Create initial haproxy.cfg overwriting existing one",
                 Run: initializeConfig,
-                Args: cobra.ExactArgs(0),
+                Args: cobra.ExactArgs(2),
         }
+
+	subCmd.PersistentFlags().StringVar(&output_dir, "dir", output_dir, "Directory, in which haproxy.cfg should be written")
 
         return subCmd
 }
 
 func initializeConfig (cmd *cobra.Command, args []string) {
+
+	lb_dns_name := args[0]
+	apiserver1 := args[1]
+
+	if len(output_dir) > 0 && output_dir[len(output_dir)-1:] != "/" {
+		output_dir = output_dir + "/"
+	}
+
+	f, err := os.Create(output_dir + "haproxy.cfg")
+	if err != nil {
+                fmt.Fprintf(os.Stderr, "Could not create \"" + output_dir + "haproxy.cfg\": %v", err)
+		os.Exit(1)
+        }
+
+	_, err = f.WriteString("global\n" +
+		"  log /dev/log daemon\n" +
+		"  maxconn 32768\n" +
+		"  chroot /var/lib/haproxy\n" +
+		"  user haproxy\n" +
+		"  group haproxy\n" +
+		"  daemon\n" +
+		"  stats socket /var/lib/haproxy/stats user haproxy group haproxy mode 0640 level operator\n" +
+		"  tune.bufsize 32768\n" +
+		"  tune.ssl.default-dh-param 2048\n" +
+		"  ssl-default-bind-ciphers ALL:!aNULL:!eNULL:!EXPORT:!DES:!3DES:!MD5:!PSK:!RC4:!ADH:!LOW@STRENGTH\n" +
+		"\n" +
+		"defaults\n" +
+		"  log     global\n" +
+		"  mode    http\n" +
+		"  option  log-health-checks\n" +
+		"  option  log-separate-errors\n" +
+		"  option  dontlog-normal\n" +
+		"  option  dontlognull\n" +
+		"  option  httplog\n" +
+		"  option  socket-stats\n" +
+		"  retries 3\n" +
+		"  option  redispatch\n" +
+		"  maxconn 10000\n" +
+		"  timeout connect     5s\n" +
+		"  timeout client     50s\n" +
+		"  timeout server    450s\n" +
+		"\n" +
+		"listen stats\n" +
+		"  bind localhost:80\n" +
+		"  stats enable\n" +
+		"  stats uri     /stats\n" +
+		"  stats refresh 5s\n" +
+		"\n")
+	if err != nil {
+                fmt.Fprintf(os.Stderr, "Writing to \"" + output_dir + "haproxy.cfg\" failed: %v", err)
+		os.Exit(1)
+        }
+
+	// XXX make writing frontend and backend an own function to add this sections later to an existing file
+	_, err = f.WriteString("frontend k8s-api\n" +
+		"    bind " + lb_dns_name + ":6443\n" +
+		"    bind localhost:6443\n" +
+		"    mode tcp\n" +
+		"    option tcplog\n" +
+		"    default_backend k8s-api\n" +
+		"\n" +
+		"backend k8s-api\n" +
+		"    mode tcp\n" +
+		"    option tcp-check\n" +
+		"    balance roundrobin\n" +
+		"    default-server inter 10s downinter 5s rise 2 fall 2 slowstart 60s maxconn 250 maxqueue 256 weight 100\n" +
+		"    server apiserver1 " + apiserver1 + ":6443 check\n\n")
+	if err != nil {
+                fmt.Fprintf(os.Stderr, "Writing to \"" + output_dir + "haproxy.cfg\" failed: %v", err)
+		os.Exit(1)
+        }
+
+	f.Close()
+
 	fmt.Printf("haproxy.cfg created\n")
 }
